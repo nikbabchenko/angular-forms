e2a1e901076127e17dfe28af597d3aa734fe2715
mbabchenko <mbabchenko@ip-172-23-109-29.us-west-2.compute.internal>
2019-11-26 17:31:54 +0200

    Adds reactive form

--- src/app/auth/components/sign-up-form-reactive/sign-up-form-reactive.component.ts
+++ src/app/auth/components/sign-up-form-reactive/sign-up-form-reactive.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RegistrationUser } from '../../models/registration-user.class';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'pm-sign-up-form-reactive',
@@ -6,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-form-reactive.component.scss']
 })
 export class SignUpFormReactiveComponent implements OnInit {
+  registrationUser = new RegistrationUser();
+  registrationForm: FormGroup;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.registrationForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required, Validators.minLength(10)]),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      withAddress: new FormControl(false),
+      city: new FormControl(),
+      street: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log('--registrationForm values', this.registrationForm.value);
+    console.log('--registrationForm model', this.registrationForm);
+  }
+
+  patchForm() {
+    this.registrationForm.patchValue({
+      firstName: 'Firstname Patched',
+      lastName: 'And LastName as well'
+    });
+  }
 }
