2233f14e8905e694d3e43b28951728559dea64ca
mbabchenko <mbabchenko@odl1905011.lohika.com>
2019-11-27 11:22:58 +0200

    moved login and signup to folders

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './pages/login.component';
-import { SignUpComponent } from './pages/sign-up.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './pages/login.component';
-import { SignUpComponent } from './pages/sign-up.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { SharedModule } from '../shared/shared.module';
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 import { SignUpFormReactiveComponent } from './components/sign-up-form-reactive/sign-up-form-reactive.component';
